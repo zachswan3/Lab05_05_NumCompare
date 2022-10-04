0951499dbb62c1d90387e5b774829e1ca2f38497
zachswan9 <swanzn@mail.uc.edu>
2022-10-04 16:39:32 -0400

    Completed task 5

---
+++ src/NumCompare.java
@@ -0,0 +1,26 @@
+public class NumCompare {
+    public static void main(String[] args) {
+
+        int num1;
+        int num2;
+
+        System.out.println("Please enter your first number");
+
+        num1 = 55;
+
+        System.out.println("Please enter your second number");
+
+        num2 = 55;
+
+        if (num1 == num2){
+            System.out.println("The numbers are equal");
+        }
+        else if (num1 < num2){
+            System.out.println("Number one is less than number 2");
+        }
+        else {
+            System.out.println("Number one is greater than number 2");
+        }
+
+    }
+}
